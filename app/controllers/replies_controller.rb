83160f59c6bab30501b050738d9d1d298d96fb9c
Jennifer Eliuk <jen7maples@gmail.com>
2013-07-14 17:57:13 -0600

    Message_id being passed to reply partial, replies saving to db when created. Controller test breaking.

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -2,8 +2,6 @@ class RepliesController < ApplicationController
 
   def create
     @reply = Reply.new(params[:reply])
-    binding.pry
-
     @message_id = Message.find(params[:reply][:message_id])
     @reply.save
     redirect_to messages_path
