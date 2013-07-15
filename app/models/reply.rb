d830cf21a2fb88143984edaefd9ec210e3829d15
Jennifer Eliuk <jen7maples@gmail.com>
2013-07-15 14:23:07 -0600

    Refactor send_reply

--- app/models/reply.rb
+++ app/models/reply.rb
@@ -6,16 +6,27 @@ class Reply < ActiveRecord::Base
   validates_presence_of :body, :message_id
 
   def send_reply
-    twilio_sid = ENV["TWILIO_SID"]
-    twilio_token = ENV["TWILIO_TOKEN"]
-    twilio_phone_number = ENV["TWILIO_PHONE_NUMBER"]
-    sender_phone_number = ENV["SENDER_PHONE_NUMBER"]
-
     twilio_client = Twilio::REST::Client.new(twilio_sid, twilio_token)
 
     twilio_client.account.sms.messages.create(
       :from => twilio_phone_number,
       :to => sender_phone_number,
       :body => body)
+  end
+
+  def twilio_sid
+    ENV["TWILIO_SID"]
+  end
+
+  def twilio_token
+    ENV["TWILIO_TOKEN"]
+  end
+
+  def twilio_phone_number
+    ENV["TWILIO_PHONE_NUMBER"]
+  end
+
+  def sender_phone_number
+    ENV["SENDER_PHONE_NUMBER"]
   end
 end
